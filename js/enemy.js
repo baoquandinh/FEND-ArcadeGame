bc9082dc15370d41b81e5fb77a27f5d9fcdc136a
Baoquan Dinh <baoquanxdinh@gmail.com>
2018-07-06 01:47:35 -0700

    Added in the render function for enemy class. This enemy class won't work because the enemy was never created. (As intended)

--- js/enemy.js
+++ js/enemy.js
@@ -2,6 +2,8 @@
 class Enemy {
     constructor(){
         this.sprite = 'images/enemy-bug.png';
+        this.x = 0;
+        this.y = 0;
     }
     // Update the enemy's position
     // Parameter: dt, a time delta between ticks
@@ -12,7 +14,7 @@ class Enemy {
     };
 
     // Draw the enemy on the screen
-    render() {
+    render(ctx) {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     };
 };
