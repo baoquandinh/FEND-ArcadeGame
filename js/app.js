6e29420d170071579ff5ab1c853e300f337d4012
Baoquan Dinh <baoquanxdinh@gmail.com>
2018-07-06 01:46:31 -0700

    Remove enemy object creation, Change "var" to "let" for es6 syntax.

--- js/app.js
+++ js/app.js
@@ -2,22 +2,19 @@
 // This class requires an update(), render() and
 // a handleInput() method.
 
-
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-let enemy = new Enemy();
-
+let player = new Player(100,100);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+    let allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
         40: 'down'
     };
-
     // player.handleInput(allowedKeys[e.keyCode]);
 });
