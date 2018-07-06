56250b76a2bad3bb5e0f7983c478bebfad2a1129
Baoquan Dinh <baoquanxdinh@gmail.com>
2018-07-06 01:47:49 -0700

    Created the player class

--- js/player.js
+++ js/player.js
@@ -1,8 +1,15 @@
 class Player {
-    constructor(){
-
+    constructor(startX, startY){
+        this.sprite = 'images/char-boy.png';
+        this.x = startX,
+        this.y = startY;
     }
-    update(){}
-    render(){}
+    update(){
+        //console.log("Updating player");
+    }
+    render(ctx){
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
     playerInput(){}
 }
+
