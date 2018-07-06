95100b2c3c96bbee4588e6bc70cb587476298fb2
Baoquan Dinh <baoquanxdinh@gmail.com>
2018-07-06 13:52:58 -0700

    Created the enemy object along with an array of enemy.

--- js/app.js
+++ js/app.js
@@ -5,7 +5,9 @@
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-let player = new Player(100,100);
+let player = new Player();
+let enemy = new Enemy(0,0);
+let allEnemies = [];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
