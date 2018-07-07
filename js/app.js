cf6583a92f319f6f3a7afa057c8b18468fee7b62
Baoquan Dinh <baoquanxdinh@gmail.com>
2018-07-06 21:42:54 -0700

    Set enemy position when starting on map

--- js/app.js
+++ js/app.js
@@ -1,12 +1,10 @@
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 let player = new Player();
-let enemy = new Enemy(0,0);
+let enemy = new Enemy(100,100);
 let allEnemies = [];
 
 // This listens for key presses and sends the keys to your
@@ -18,5 +16,5 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-    // player.handleInput(allowedKeys[e.keyCode]);
+    player.handleInput(allowedKeys[e.keyCode]);
 });
